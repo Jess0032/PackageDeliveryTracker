6031464514629873520d584641f98107b33e5724
Jess0032 <75321858+Jess0032@users.noreply.github.com>
2022-07-22 18:49:25 -0400

    Dubugg

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ API_ID: int = int(os.getenv("API_ID"))
 API_HASH: str = os.getenv("API_HASH")
 BOT_TOKEN: str = os.environ.get("BOT_TOKEN")
 HOURS: int = int(os.environ.get("HOURS"))
+ADMIN: int = int(os.environ.get("ADMIN"))
 
 db = DBHelper(os.getenv("DATABASE_URL"))
 
